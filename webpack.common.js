b1b9c05eb04e7a33056890202bc33caf37117cd3
Bartłomiej Pawlak <bartek.pawlak56@gmail.com>
2020-09-09 10:57:58 +0200

    First Commit - home page partially coded

---
+++ webpack.common.js
@@ -0,0 +1,27 @@
+const path = require("path");
+
+module.exports = {
+  entry: ['babel-polyfill', './src/js/index.js'],
+  module: {
+    rules: [
+      {
+        test: /\.html$/,
+        use: ["html-loader"]
+      },
+      {
+        test: /\.(ico|svg|png|jpg|gif)$/,
+        use: {
+          loader: "file-loader",
+          options: {
+            name: "[name].[ext]",
+            outputPath: "assets/img/",
+            publicPath: 'assets/img/'
+          }
+        }
+      },
+    ]
+  },
+  externals: {
+    moment: 'moment'
+  }
+};
---
+++ webpack.dev.js
@@ -0,0 +1,50 @@
+const path = require("path");
+const common = require("./webpack.common");
+const merge = require("webpack-merge");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyPlugin = require('copy-webpack-plugin');
+const FaviconsWebpackPlugin = require('favicons-webpack-plugin')
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+module.exports = merge(common, {
+  mode: "development",
+  output: {
+    filename: "js/[name].bundle.js",
+    path: path.resolve(__dirname, "dist")
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html"
+    }),
+    new CopyPlugin({
+      patterns: [
+        {
+          from: './src/img/_sprite.svg',
+          to: 'assets/img/', // relative path in output directory
+        }
+      ]
+    }),
+    // new FaviconsWebpackPlugin('./src/img/favicon.ico'),
+    new ExtractTextPlugin("style.css")
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: ExtractTextPlugin.extract({
+          use: [
+            'css-loader',
+            'sass-loader'
+          ]
+        })
+      },
+      // {
+      //   test: /\.css$/,
+      //   use: [
+      //     "style-loader", //3. Inject styles into DOM
+      //     "css-loader", //2. Turns css into commonjs
+      //   ]
+      // }
+    ]
+  }
+});
