6a995a815526f172c4f820d2fc6813b2652e64d4
Bartłomiej Pawlak <bartek.pawlak56@gmail.com>
2020-09-19 17:13:54 +0200

    Cta buttons animated and running

--- src/js/index.js
+++ src/js/index.js
@@ -6,32 +6,9 @@ gsap.registerPlugin(ScrollTrigger);
 
 const sections = ['home', 'portfolio', 'about', 'contact'];
 
-gsap.to('.home__paint', {
-    height: "100%",
-    duration: 1.5,
-    ease: "power2.out"
-});
+let tl = gsap.timeline({ defaults: { duration: 1.5 } });
 
-gsap.from(".home__fig", {
-    opacity: 0,
-    duration: 1.5,
-    y: -120,
-    ease: "elastic(1, 0.3)",
-    delay: .5
-});
-
-gsap.from(".hero", {
-    opacity: 0,
-    duration: 1.5,
-    y: 40,
-    ease: "elastic(1, 0.3)",
-    delay: .8
-});
-
-gsap.from(".home__links", {
-    opacity: 0,
-    duration: 1.5,
-    x: -50,
-    ease: "elastic(1, 0.3)",
-    delay: 1
-});
+tl.to('.home__paint', { height: "100%", ease: "power2.out" })
+    .from(".home__fig", { opacity: 0, y: -120, ease: "elastic(1, 0.3)" }, "-=1")
+    .from(".hero", { opacity: 0, duration: 1.5, y: 40, ease: "elastic(1, 0.3)" }, "-=1.2")
+    .from(".home__links", { opacity: 0, duration: 1.5, x: -50, ease: "elastic(1, 0.3)" }, "-=1.3");
