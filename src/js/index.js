80f3c35c0b1172eb08884db0d5a141fe0eb3fb24
Bartłomiej Pawlak <bartek.pawlak56@gmail.com>
2020-09-21 17:45:34 +0200

    Portfolio section restyled, animation applied, it's kinda neat if you ask me

--- src/js/index.js
+++ src/js/index.js
@@ -7,12 +7,43 @@ import { projects } from './views/base.js';
 import * as portfolioView from "./views/portfolioView.js";
 
 
-// portfolioView.renderProjects(projects
+//Render Portfolio projects
 projects.forEach(e => portfolioView.renderProjects(e));
 
+// Home Animation
 let tl = gsap.timeline({ defaults: { duration: 1.5 } });
 
 tl.to('.home__paint', { height: "100%", ease: "power2.out" })
     .from(".home__fig", { opacity: 0, y: -120, ease: "elastic(1, 0.3)" }, "-=1")
     .from(".hero", { opacity: 0, duration: 1.5, y: 40, ease: "elastic(1, 0.3)" }, "-=1.2")
     .from(".home__links", { opacity: 0, duration: 1.5, x: -50, ease: "elastic(1, 0.3)" }, "-=1.3");
+
+
+//Portfolio Animation
+
+gsap.from(".portfolio__header", {
+    x: -50, opacity: 0, ease: "elastic(1, 0.3)", duration: 1.5, scrollTrigger: {
+        trigger: '.portfolio',
+        start: "top 80%"
+    }
+});
+
+gsap.fromTo(".portfolio", { backgroundSize: "4%" }, {
+    backgroundSize: "60%", duration: 1, ease: "easeIn", scrollTrigger: {
+        trigger: ".portfolio",
+        start: "5px bottom",
+        scrub: 1
+    }
+})
+
+const projectsMrkp = document.querySelectorAll('.project');
+projectsMrkp.forEach(e => {
+    gsap.from(e, {
+        y: -100, opacity: 0, ease: "elastic(1, 0.3)", duration: 1.5, scrollTrigger: {
+            trigger: e,
+            start: "-10% center"
+        }
+    })
+});
+
+// About Me Animation
