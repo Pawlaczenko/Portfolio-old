f794d3fe44835963833de6396a9aacf41814f04c
Bartłomiej Pawlak <bartek.pawlak56@gmail.com>
2020-09-23 12:16:27 +0200

    Contact section animation ready to go!

--- src/js/index.js
+++ src/js/index.js
@@ -60,5 +60,21 @@ const aboutInfo = document.querySelector(".about__info");
 aboutTl.from(".about__picture", { borderWidth: "0", ease: "easeOut", duration: 1 })
     .from(".about__header", { x: 50, opacity: 0, ease: "elastic(1, 0.3)", duration: 1.5 }, "-=.7")
     .from(aboutInfo.children, { opacity: 0, x: 40, ease: "elastic(1, 0.3)", duration: 1.5, stagger: .25 }, "-=1.2")
-    .from(".about", { backgroundPosition: "120%", ease: "ease", duration: "1.5" }, "-=1.2")
+    .fromTo(".about", { backgroundPositionX: "120%" }, { backgroundPositionX: "100%", ease: "power2.out", duration: "1.5" }, "-=1.4")
     .from(".about__lists", { y: 10, opacity: 0, ease: "easeInOut", duration: 1 }, "-=.8");
+
+// Contact animation
+
+let contactTl = gsap.timeline({
+    scrollTrigger: {
+        trigger: ".contact",
+        start: "top 70%"
+    }
+});
+
+const formEl = document.querySelector(".contact__form");
+const infoEl = document.querySelector(".contact__options");
+
+contactTl.from(".contact__header", { opacity: 0, x: -50, duration: 1.5, ease: "elastic(1, 0.3)" })
+    .from(formEl.children, { y: 10, opacity: 0, duration: 1, ease: "easeInOut)", stagger: .2 }, "-=1.2")
+    .from(infoEl.children, { y: 10, opacity: 0, duration: 1, ease: "easeInOut)", stagger: .2 }, "-=.95");
