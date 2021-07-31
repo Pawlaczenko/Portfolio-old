3605357cc3d69a3e162b9d56e26c1019d3bfe9ee
Bartłomiej Pawlak <bartek.pawlak56@gmail.com>
2021-07-31 19:36:18 +0200

    Content added

--- src/js/views/base.js
+++ src/js/views/base.js
@@ -5,42 +5,52 @@ export const HREFS = {
 }
 
 export const sections = ['home', 'portfolio', 'about', 'contact'];
-export const projects = [{
-    title: 'Tempo',
-    date: 'July 2020',
-    desc: 'Tempo is a website where you can test your typing speed using song lyrics.',
-    img: 'tempo.jpg',
-    techs: ['JavaScript', 'SCSS', 'HTML', 'Webpack'],
-    liveLink: "Tempo-ghp",
-    gitLink: "Tempo"
-},
-{
-    title: 'MoviesBLOG',
-    date: 'October - December 2019',
-    desc: 'I developed MoviesBlog as the ending project in my highschool. I wrote both frond and back end.',
-    img: 'moviesblog.jpg',
-    techs: ['JavaScript', 'SCSS', 'HTML', 'PHP', 'MySQL'],
-    liveLink: "moviesBlog",
-    gitLink: "moviesBlog"
-},
-{
-    title: 'pejper Soker',
-    date: 'December 2018 - June 2019',
-    desc: 'Group project. Game based on a well known time killer Paper Soccer. It contains of singleplayer and multiplayer modes.',
-    img: 'pejpersoker.jpg',
-    techs: ['JavaScript', 'CSS', 'HTML', 'PHP', 'MySQL'],
-    liveLink: "pejper_soker/index.html",
-    gitLink: "PejperSoker"
-},
-{
-    title: 'Pacman',
-    date: 'December 2017 - May 2018',
-    desc: 'Group project. Developed using p5.js library. Works on canvas.',
-    img: 'pacman.jpg',
-    techs: ['JavaScript', 'SCSS', 'HTML', 'p5.js'],
-    liveLink: "pacman",
-    gitLink: "PacMan"
-}];
+export const projects = [
+    {
+        title: 'Tempo',
+        date: 'July 2020',
+        desc: 'Tempo is a website where you can test your typing speed using song lyrics.',
+        img: 'tempo.jpg',
+        techs: ['JavaScript', 'SCSS', 'HTML', 'Webpack'],
+        liveLink: "Tempo-ghp",
+        gitLink: "Tempo"
+    },
+    {
+        title: 'Earthly',
+        date: 'April 2021',
+        desc: 'Earthly is a simple one-page website made as a final project for the semester.',
+        img: 'earthly.jpg',
+        techs: ['JavaScript', 'CSS', 'HTML'],
+        liveLink: "Earthly",
+        gitLink: "Earthly"
+    },
+    {
+        title: 'MoviesBLOG',
+        date: 'October - December 2019',
+        desc: 'I developed MoviesBlog as the ending project in my highschool. I wrote both frond and back end.',
+        img: 'moviesblog.jpg',
+        techs: ['JavaScript', 'SCSS', 'HTML', 'PHP', 'MySQL'],
+        liveLink: "moviesBlog",
+        gitLink: "moviesBlog"
+    },
+    {
+        title: 'pejper Soker',
+        date: 'December 2018 - June 2019',
+        desc: 'Group project. Game based on a well known time killer Paper Soccer. It contains of singleplayer and multiplayer modes.',
+        img: 'pejpersoker.jpg',
+        techs: ['JavaScript', 'CSS', 'HTML', 'PHP', 'MySQL'],
+        liveLink: "pejper_soker/index.html",
+        gitLink: "PejperSoker"
+    },
+    {
+        title: 'Pacman',
+        date: 'December 2017 - May 2018',
+        desc: 'Group project. Developed using p5.js library. Works on canvas.',
+        img: 'pacman.jpg',
+        techs: ['JavaScript', 'SCSS', 'HTML', 'p5.js'],
+        liveLink: "pacman",
+        gitLink: "PacMan"
+    }];
 
 export const ajax = (method, url, data, success, error) => {
     var xhr = new XMLHttpRequest();
