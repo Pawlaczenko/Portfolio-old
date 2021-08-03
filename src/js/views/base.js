9e4f8824f332af08b041e3b9548ad8a80b2e1117
Bartłomiej Pawlak <bartek.pawlak56@gmail.com>
2021-08-03 17:19:22 +0200

    Changed the projects descriptions + minor style tweaks in Portfolio section

--- src/js/views/base.js
+++ src/js/views/base.js
@@ -9,7 +9,7 @@ export const projects = [
     {
         title: 'Tempo',
         date: 'July 2020',
-        desc: 'Tempo is a website where you can test your typing speed using song lyrics.',
+        desc: 'Tempo is a website where you can test your typing speed using song lyrics. I developed it to test my abilities in JavaScript. It uses the external API with song lyrics.',
         img: 'tempo.jpg',
         techs: ['JavaScript', 'SCSS', 'HTML', 'Webpack'],
         liveLink: "Tempo-ghp",
@@ -18,7 +18,7 @@ export const projects = [
     {
         title: 'Earthly',
         date: 'April 2021',
-        desc: 'Earthly is a simple one-page website made as a final project for the semester.',
+        desc: 'Earthly is a simple one-page website made as a final project for the first semester at my university. To develop it I used plain HTML + CSS + JavaScript workflow.',
         img: 'earthly.jpg',
         techs: ['JavaScript', 'CSS', 'HTML'],
         liveLink: "Earthly",
@@ -27,7 +27,7 @@ export const projects = [
     {
         title: 'MoviesBLOG',
         date: 'October - December 2019',
-        desc: 'I developed MoviesBlog as the final project in my highschool. I wrote both front and back end.',
+        desc: "I developed MoviesBlog as the final project in my high school. I wrote both front and back end. This was made mainly to test my PHP and SQL knowledge. It's a website where you can add and manage posts.",
         img: 'moviesblog.jpg',
         techs: ['JavaScript', 'SCSS', 'HTML', 'PHP', 'MySQL'],
         liveLink: "-1",
@@ -36,7 +36,7 @@ export const projects = [
     {
         title: 'pejper Soker',
         date: 'December 2018 - June 2019',
-        desc: 'Group project. Game based on a well known time killer Paper Soccer. It contains of singleplayer and multiplayer modes.',
+        desc: 'Group project. The game is based on a well-known time killer Paper Soccer. Making this project was a great opportunity for me and my colleagues to learn the Git workflow. You play against the bot called Shrek. Your goal is to score a goal.',
         img: 'pejpersoker.jpg',
         techs: ['JavaScript', 'CSS', 'HTML', 'PHP', 'MySQL'],
         liveLink: "PejperSoker/",
@@ -45,7 +45,7 @@ export const projects = [
     {
         title: 'Pacman',
         date: 'December 2017 - May 2018',
-        desc: 'Group project. Developed using p5.js library. Works on canvas.',
+        desc: 'Group project. Developed using p5.js library. It is our take on a popular game Pacman.',
         img: 'pacman.jpg',
         techs: ['JavaScript', 'SCSS', 'HTML', 'p5.js'],
         liveLink: "PacMan",
